6bcfcf6793425915b80b145a3c2160b17fc40b88
Miguel Carlo Fernandez <miguelcarlofernandez@yahoo.com>
2023-05-24 20:25:27 +0800

    Revert "removed multiply"
    This reverts commit c83b3c02c65811e52f1c39c09a52862224447320.

--- Jarren.py
+++ Jarren.py
@@ -2,6 +2,9 @@ def add(x, y):
     sum = x + y
     return sum
 
+def multiply(x, y):
+    product = x * y
+    return product
 
 def subtract(x, y):
     diff = x - y
@@ -13,9 +16,11 @@ def square_function(x):
 
 def main():
     sum = add(2, 4)
+    product = multiply(4, 4)
     diff = subtract(8, 4)
     square = square_function(4)
     print(sum)
+    print(product)
     print(diff)
     print(square)
 
